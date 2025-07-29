e46773bda8b81f52151c4250f69fa38d72a711f3
PlanProcCommMan <james.baker@planetaryprocessing.io>
2025-07-29 16:11:13 +0100

    Initial branch

--- SDKNode.cs
+++ SDKNode.cs
@@ -6,261 +6,261 @@ using Planetary;
 
 public partial class SDKNode : Node
 {	
-	private SDK sdk;
-	private ulong gameId;
-	private Godot.Collections.Dictionary<string, Godot.Variant> chunkMap = new Godot.Collections.Dictionary<string, Godot.Variant>();
-	private Action<Dictionary<String, object>> eventCallback;
+    private SDK sdk;
+    private ulong gameId;
+    private Godot.Collections.Dictionary<string, Godot.Variant> chunkMap = new Godot.Collections.Dictionary<string, Godot.Variant>();
+    private Action<Dictionary<String, object>> eventCallback;
 
-	public uint chunkSize;
-	public bool test;
-	
-	
-	public void SetEventCallback(GodotObject callbackObject)
-	{
-		if (callbackObject != null && callbackObject.HasMethod("server_to_client"))
-		{
-			GD.Print("Registered server_to_client event message");
+    public uint chunkSize;
+    public bool test;
+    
+    
+    public void SetEventCallback(GodotObject callbackObject)
+    {
+        if (callbackObject != null && callbackObject.HasMethod("server_to_client"))
+        {
+            GD.Print("Registered server_to_client event message");
 
-			this.eventCallback = (Dictionary<string, object> data) =>
-			{
-				callbackObject.Call("server_to_client", ConvertToGodotVariantDictionary(data));
-			};
-		}
-		else
-		{
-			GD.PrintErr("Provided object does not have 'server_to_client' event message");
-		}
+            this.eventCallback = (Dictionary<string, object> data) =>
+            {
+                callbackObject.Call("server_to_client", ConvertToGodotVariantDictionary(data));
+            };
+        }
+        else
+        {
+            GD.PrintErr("Provided object does not have 'server_to_client' event message");
+        }
 }
-	
-	public void SetGameID(ulong gameId)
-	{
-		this.gameId = gameId;
-		if (this.eventCallback != null){
-			sdk = new SDK(
-				gameId,
-				this.HandleChunk,
-				this.eventCallback
-			);
-		} else {
-			sdk = new SDK(
-				gameId,
-				this.HandleChunk
-			);
-		}
-		GD.Print("SDKNode game ID " + gameId);
-	}
-	
-	public string Connect(string username, string password)
-	{
-		try 
-		{
-			sdk.Connect(username, password);
-			GD.Print("Player " + username + " authenticated on game ID " + gameId);
-			sdk.Join();
-			return "";
-		}
-		catch (Exception e)
-		{
-			return e.Message;
-		}
-	}
-	
-	public bool GetIsConnected()
-	{
-		return sdk.IsConnected();
-	}
-	
-	public string GetUUID()
-	{
-		return sdk.UUID;
-	}
-	
-	public void Message(Godot.Collections.Dictionary<string, Godot.Variant> msg)
-	{
-		Dictionary<string, dynamic> message = new Dictionary<string, dynamic>();
-		foreach (var key in msg.Keys)
-		{
-			dynamic value = msg[key];
-			message[key] = ConvertFromGodotVariant(value);
-		}
-		sdk.Message(message);
-	}
-	
-	public void DirectMessage(string TargetUUID, Godot.Collections.Dictionary<string, Godot.Variant> msg)
-	{
-		Dictionary<string, dynamic> message = new Dictionary<string, dynamic>();
-		foreach (var key in msg.Keys)
-		{
-			dynamic value = msg[key];
-			message[key] = ConvertFromGodotVariant(value);
-		}
-		sdk.DirectMessage(TargetUUID, message);
-	}
-	
-	private dynamic ConvertFromGodotVariant(Godot.Variant value)
-	{
-		switch (value.VariantType)
-		{
-			case Godot.Variant.Type.Nil:
-				return null;
-			case Godot.Variant.Type.Bool:
-				return value.As<bool>();
-			case Godot.Variant.Type.Int:
-				return value.As<long>();
-			case Godot.Variant.Type.Float:
-				return value.As<double>();
-			case Godot.Variant.Type.String:
-				return value.As<string>();
-			 case Godot.Variant.Type.Array:
-				var godotArray = value.As<Godot.Collections.Array<Godot.Variant>>();
-				var csharpList = new List<dynamic>();
-				foreach (var item in godotArray)
-				{
-					csharpList.Add(ConvertFromGodotVariant(item));
-				}
-				return csharpList;
-			case Godot.Variant.Type.Dictionary:
-				var godotDict = value.As<Godot.Collections.Dictionary<string, Godot.Variant>>();
-				var csharpDict = new Dictionary<string, dynamic>();
-				foreach (var dictKey in godotDict.Keys)
-				{
-					dynamic dictValue = ConvertFromGodotVariant(godotDict[dictKey]);
-					csharpDict[dictKey] = dictValue;
-				}
-				return csharpDict;
-			default:
-				return null;
-		}
-	}
+    
+    public void SetGameID(ulong gameId)
+    {
+        this.gameId = gameId;
+        if (this.eventCallback != null){
+            sdk = new SDK(
+                gameId,
+                this.HandleChunk,
+                this.eventCallback
+            );
+        } else {
+            sdk = new SDK(
+                gameId,
+                this.HandleChunk
+            );
+        }
+        GD.Print("SDKNode game ID " + gameId);
+    }
+    
+    public string Connect(string username, string password)
+    {
+        try 
+        {
+            sdk.Connect(username, password);
+            GD.Print("Player " + username + " authenticated on game ID " + gameId);
+            sdk.Join();
+            return "";
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+    public bool GetIsConnected()
+    {
+        return sdk.IsConnected();
+    }
+    
+    public string GetUUID()
+    {
+        return sdk.UUID;
+    }
+    
+    public void Message(Godot.Collections.Dictionary<string, Godot.Variant> msg)
+    {
+        Dictionary<string, dynamic> message = new Dictionary<string, dynamic>();
+        foreach (var key in msg.Keys)
+        {
+            dynamic value = msg[key];
+            message[key] = ConvertFromGodotVariant(value);
+        }
+        sdk.Message(message);
+    }
+    
+    public void DirectMessage(string TargetUUID, Godot.Collections.Dictionary<string, Godot.Variant> msg)
+    {
+        Dictionary<string, dynamic> message = new Dictionary<string, dynamic>();
+        foreach (var key in msg.Keys)
+        {
+            dynamic value = msg[key];
+            message[key] = ConvertFromGodotVariant(value);
+        }
+        sdk.DirectMessage(TargetUUID, message);
+    }
+    
+    private dynamic ConvertFromGodotVariant(Godot.Variant value)
+    {
+        switch (value.VariantType)
+        {
+            case Godot.Variant.Type.Nil:
+                return null;
+            case Godot.Variant.Type.Bool:
+                return value.As<bool>();
+            case Godot.Variant.Type.Int:
+                return value.As<long>();
+            case Godot.Variant.Type.Float:
+                return value.As<double>();
+            case Godot.Variant.Type.String:
+                return value.As<string>();
+             case Godot.Variant.Type.Array:
+                var godotArray = value.As<Godot.Collections.Array<Godot.Variant>>();
+                var csharpList = new List<dynamic>();
+                foreach (var item in godotArray)
+                {
+                    csharpList.Add(ConvertFromGodotVariant(item));
+                }
+                return csharpList;
+            case Godot.Variant.Type.Dictionary:
+                var godotDict = value.As<Godot.Collections.Dictionary<string, Godot.Variant>>();
+                var csharpDict = new Dictionary<string, dynamic>();
+                foreach (var dictKey in godotDict.Keys)
+                {
+                    dynamic dictValue = ConvertFromGodotVariant(godotDict[dictKey]);
+                    csharpDict[dictKey] = dictValue;
+                }
+                return csharpDict;
+            default:
+                return null;
+        }
+    }
 
-	private Godot.Variant ConvertToGodotVariant(dynamic value)
-	{
-		if (value == null)
-		{
-			return new Godot.Variant(); // Return a null Variant
-		}
+    private Godot.Variant ConvertToGodotVariant(dynamic value)
+    {
+        if (value == null)
+        {
+            return new Godot.Variant(); // Return a null Variant
+        }
 
-		Type valueType = value.GetType();
+        Type valueType = value.GetType();
 
-		bool isDict = valueType.IsGenericType && valueType.GetGenericTypeDefinition() == typeof(Dictionary<,>);
-		if (isDict)
-		{
-			var gdDict = new Godot.Collections.Dictionary<Godot.Variant, Godot.Variant>();
-			foreach ((dynamic key, dynamic val) in (Dictionary<object, object>)value)
-			{
-				gdDict[key] = ConvertToGodotVariant(val);
-			}
-			return gdDict;
-		}
+        bool isDict = valueType.IsGenericType && valueType.GetGenericTypeDefinition() == typeof(Dictionary<,>);
+        if (isDict)
+        {
+            var gdDict = new Godot.Collections.Dictionary<Godot.Variant, Godot.Variant>();
+            foreach ((dynamic key, dynamic val) in (Dictionary<object, object>)value)
+            {
+                gdDict[key] = ConvertToGodotVariant(val);
+            }
+            return gdDict;
+        }
 
-		bool isArray = value is List<dynamic> || value is Array;
-		if (isArray)
-		{
-			var gdArray = new Godot.Collections.Array<Godot.Variant>();
-			foreach (var item in value)
-			{
-				gdArray.Add(ConvertToGodotVariant(item));
-			}
-			return gdArray;
-		}
+        bool isArray = value is List<dynamic> || value is Array;
+        if (isArray)
+        {
+            var gdArray = new Godot.Collections.Array<Godot.Variant>();
+            foreach (var item in value)
+            {
+                gdArray.Add(ConvertToGodotVariant(item));
+            }
+            return gdArray;
+        }
 
-		return value;
-	}
+        return value;
+    }
 
 
-	
-	private Godot.Collections.Dictionary<string, Godot.Variant> ConvertToGodotVariantDictionary(Dictionary<string, dynamic> dict)
-	{
-		var gdDict = new Godot.Collections.Dictionary<string, Godot.Variant>();
-		foreach (var kvp in dict)
-		{
-			gdDict[kvp.Key] = ConvertToGodotVariant(kvp.Value);
-		}
-		return gdDict;
-	}
+    
+    private Godot.Collections.Dictionary<string, Godot.Variant> ConvertToGodotVariantDictionary(Dictionary<string, dynamic> dict)
+    {
+        var gdDict = new Godot.Collections.Dictionary<string, Godot.Variant>();
+        foreach (var kvp in dict)
+        {
+            gdDict[kvp.Key] = ConvertToGodotVariant(kvp.Value);
+        }
+        return gdDict;
+    }
 
-	public void Update()
-	{
-		sdk.Update();
-		if (!sdk.entities.ContainsKey(sdk.UUID)) {
-			sdk.Join();
-		}
-	}
-	
-	public Godot.Collections.Dictionary<string, Godot.Variant> GetEntities()
-	{
-		Godot.Collections.Dictionary<string, Godot.Variant> gdEntities = new Godot.Collections.Dictionary<string, Godot.Variant>();
+    public void Update()
+    {
+        sdk.Update();
+        if (!sdk.entities.ContainsKey(sdk.UUID)) {
+            sdk.Join();
+        }
+    }
+    
+    public Godot.Collections.Dictionary<string, Godot.Variant> GetEntities()
+    {
+        Godot.Collections.Dictionary<string, Godot.Variant> gdEntities = new Godot.Collections.Dictionary<string, Godot.Variant>();
 
-		foreach (var pair in sdk.entities)
-		{
-			Entity entity = pair.Value;
-			Godot.Collections.Dictionary<string, Godot.Variant> gdEntity = new Godot.Collections.Dictionary<string, Godot.Variant>();
-			gdEntity["x"] = entity.x;
-			gdEntity["y"] = entity.y;
-			gdEntity["z"] = entity.z;
-			gdEntity["data"] = ConvertToGodotVariantDictionary(entity.data);
-			gdEntity["type"] = entity.type;
-			gdEntities[entity.id] = gdEntity;
-		}
-		return gdEntities;
-	}
+        foreach (var pair in sdk.entities)
+        {
+            Entity entity = pair.Value;
+            Godot.Collections.Dictionary<string, Godot.Variant> gdEntity = new Godot.Collections.Dictionary<string, Godot.Variant>();
+            gdEntity["x"] = entity.x;
+            gdEntity["y"] = entity.y;
+            gdEntity["z"] = entity.z;
+            gdEntity["data"] = ConvertToGodotVariantDictionary(entity.data);
+            gdEntity["type"] = entity.type;
+            gdEntities[entity.id] = gdEntity;
+        }
+        return gdEntities;
+    }
 
-	private void HandleChunk(Chunk cnk) {
-		/*
-		Function passed to SDK as part of init (or closest proxy to, SetGameId) 
-		*/
-		if (!chunkMap.ContainsKey(cnk.id.ToString())) {
-		  // Create a new dictionary to represent the chunk
-			Godot.Collections.Dictionary<string, Godot.Variant> gdChunk = new Godot.Collections.Dictionary<string, Godot.Variant>();
+    private void HandleChunk(Chunk cnk) {
+        /*
+        Function passed to SDK as part of init (or closest proxy to, SetGameId) 
+        */
+        if (!chunkMap.ContainsKey(cnk.id.ToString())) {
+          // Create a new dictionary to represent the chunk
+            Godot.Collections.Dictionary<string, Godot.Variant> gdChunk = new Godot.Collections.Dictionary<string, Godot.Variant>();
 
-			// Set the gdChunk data
-			gdChunk["data"] = ConvertToGodotVariantDictionary(cnk.data);
-			gdChunk["x"] = cnk.x;
-			gdChunk["y"] = cnk.y;
-			gdChunk["id"] = cnk.id.ToString();
+            // Set the gdChunk data
+            gdChunk["data"] = ConvertToGodotVariantDictionary(cnk.data);
+            gdChunk["x"] = cnk.x;
+            gdChunk["y"] = cnk.y;
+            gdChunk["id"] = cnk.id.ToString();
 
-			// Add the new chunk to the map
-			chunkMap[cnk.id.ToString()] = gdChunk;
+            // Add the new chunk to the map
+            chunkMap[cnk.id.ToString()] = gdChunk;
 
-			GD.Print($"Added new chunk with ID {cnk.id}, x=({cnk.x}, y={cnk.y})");
-		} else {
-			// The chunk already exists, update its data if necessary
-			Godot.Variant existingChunkVariant = chunkMap[cnk.id.ToString()];
-			 if (existingChunkVariant.Obj is Godot.Collections.Dictionary existingChunk) {
-				// Update chunk data if needed
-				existingChunk["data"] = ConvertToGodotVariantDictionary(cnk.data);
-				GD.Print($"Updated existing chunk with ID {cnk.id}");
-			} else {
-				GD.PrintErr($"Failed to update chunk with ID {cnk.id}: existing chunk is null or invalid.");
-			}
-		}
-		// Remove chunks further than 3 from the player's current chunk
-		List<string> toRemove = new List<string>();
-		foreach (string chunk_id in chunkMap.Keys) {
-			// Get the chunk from the dictionary
-			if (!chunkMap.TryGetValue(chunk_id, out Godot.Variant savedChunkVariant)) {
-				GD.PrintErr($"Chunk ID {chunk_id} not found in chunkMap");
-				continue;
-			}
-			var savedChunk = savedChunkVariant.As<Godot.Collections.Dictionary<string, Godot.Variant>>();
-			if (savedChunk != null) {
-				if (Math.Abs((int)savedChunk["x"] - cnk.x) > 3 || Math.Abs((int)(savedChunk["y"]) - cnk.y) > 3) {
-					toRemove.Add(chunk_id);
-				}
-			} else {
-				GD.PrintErr($"Error - Failed to extract dictionary for chunk {chunk_id}.");
-			}
-		}
+            GD.Print($"Added new chunk with ID {cnk.id}, x=({cnk.x}, y={cnk.y})");
+        } else {
+            // The chunk already exists, update its data if necessary
+            Godot.Variant existingChunkVariant = chunkMap[cnk.id.ToString()];
+             if (existingChunkVariant.Obj is Godot.Collections.Dictionary existingChunk) {
+                // Update chunk data if needed
+                existingChunk["data"] = ConvertToGodotVariantDictionary(cnk.data);
+                GD.Print($"Updated existing chunk with ID {cnk.id}");
+            } else {
+                GD.PrintErr($"Failed to update chunk with ID {cnk.id}: existing chunk is null or invalid.");
+            }
+        }
+        // Remove chunks further than 3 from the player's current chunk
+        List<string> toRemove = new List<string>();
+        foreach (string chunk_id in chunkMap.Keys) {
+            // Get the chunk from the dictionary
+            if (!chunkMap.TryGetValue(chunk_id, out Godot.Variant savedChunkVariant)) {
+                GD.PrintErr($"Chunk ID {chunk_id} not found in chunkMap");
+                continue;
+            }
+            var savedChunk = savedChunkVariant.As<Godot.Collections.Dictionary<string, Godot.Variant>>();
+            if (savedChunk != null) {
+                if (Math.Abs((int)savedChunk["x"] - cnk.x) > 3 || Math.Abs((int)(savedChunk["y"]) - cnk.y) > 3) {
+                    toRemove.Add(chunk_id);
+                }
+            } else {
+                GD.PrintErr($"Error - Failed to extract dictionary for chunk {chunk_id}.");
+            }
+        }
 
-		// Remove the chunks marked for removal
-		foreach (string chunkId in toRemove) {
-			chunkMap.Remove(chunkId);
-		}
-	}
-	
-	public Godot.Collections.Dictionary<string, Godot.Variant> GetChunks()
-	{
-		return chunkMap;
-	}
-	
+        // Remove the chunks marked for removal
+        foreach (string chunkId in toRemove) {
+            chunkMap.Remove(chunkId);
+        }
+    }
+    
+    public Godot.Collections.Dictionary<string, Godot.Variant> GetChunks()
+    {
+        return chunkMap;
+    }
+    
 }
--- pp_utils.gd
+++ pp_utils.gd
@@ -2,110 +2,110 @@ static var reference_tag = ""
 static var reference_hint = ""
 
 static func _init_static_variables():
-	reference_tag = "<Reference Include=\"Planetary\">"
-	reference_hint = "<HintPath>addons/planetary_processing/sdk/csharp-sdk.dll</HintPath>"
+    reference_tag = "<Reference Include=\"Planetary\">"
+    reference_hint = "<HintPath>addons/planetary_processing/sdk/csharp-sdk.dll</HintPath>"
 
 static func write_bytes_to_file(file_path, bytes):
-	var directory_path = file_path.get_base_dir()
-	DirAccess.make_dir_recursive_absolute(directory_path)
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	if FileAccess.get_open_error():
-		assert(false, "Error find_files_by_extension file: " + file_path)
-	file.store_buffer(bytes)
-	file.close()
-	print("Stored file: ", file_path)
+    var directory_path = file_path.get_base_dir()
+    DirAccess.make_dir_recursive_absolute(directory_path)
+    var file = FileAccess.open(file_path, FileAccess.WRITE)
+    if FileAccess.get_open_error():
+        assert(false, "Error find_files_by_extension file: " + file_path)
+    file.store_buffer(bytes)
+    file.close()
+    print("Stored file: ", file_path)
 
 static func write_string_to_file(file_path, content):
-	var directory_path = file_path.get_base_dir()
-	DirAccess.make_dir_recursive_absolute(directory_path)
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	if FileAccess.get_open_error():
-		assert(false, "Error opening file: " + file_path)
-	file.store_string(content)
-	file.close()
-	print("Stored file: ", file_path)
+    var directory_path = file_path.get_base_dir()
+    DirAccess.make_dir_recursive_absolute(directory_path)
+    var file = FileAccess.open(file_path, FileAccess.WRITE)
+    if FileAccess.get_open_error():
+        assert(false, "Error opening file: " + file_path)
+    file.store_string(content)
+    file.close()
+    print("Stored file: ", file_path)
 
 static func refresh_filesystem():
-	if Engine.is_editor_hint():
-		var _editor_interface = Engine.get_singleton("EditorInterface")
-		var resource_filesystem = _editor_interface.get_resource_filesystem()
-		resource_filesystem.scan()
+    if Engine.is_editor_hint():
+        var _editor_interface = Engine.get_singleton("EditorInterface")
+        var resource_filesystem = _editor_interface.get_resource_filesystem()
+        resource_filesystem.scan()
 
 static func scrub_lua_files(path):
-	var dir = DirAccess.open(path)
-	for file_name in dir.get_files():
-		if file_name.ends_with(".lua"):
-			dir.remove(file_name)
-			print("Deleted file: ", file_name)
+    var dir = DirAccess.open(path)
+    for file_name in dir.get_files():
+        if file_name.ends_with(".lua"):
+            dir.remove(file_name)
+            print("Deleted file: ", file_name)
 
 static func zip_directory(path, file_path) -> Error:
-	DirAccess.remove_absolute(file_path)
-	var zip_packer := ZIPPacker.new()
-	var err := zip_packer.open(file_path, ZIPPacker.APPEND_CREATE)
-	if err != OK:
-		return err
+    DirAccess.remove_absolute(file_path)
+    var zip_packer := ZIPPacker.new()
+    var err := zip_packer.open(file_path, ZIPPacker.APPEND_CREATE)
+    if err != OK:
+        return err
 
-	err = add_files_to_zip(zip_packer, path, "")
+    err = add_files_to_zip(zip_packer, path, "")
 
-	zip_packer.close()
-	return err
+    zip_packer.close()
+    return err
 
 static func add_files_to_zip(zip_packer, directory_path, relative_path) -> Error:
-	var dir = DirAccess.open(directory_path)
-	for file_name in dir.get_files():
-		var file_path = relative_path + file_name
-		print(file_path)
-		var absolute_file_path = directory_path + "/" + file_name
-		var err = zip_packer.start_file(file_path)
-		if err != OK:
-			return err
-		
-		var file := FileAccess.open(absolute_file_path, FileAccess.READ)
-		if file:
-			zip_packer.write_file(FileAccess.get_file_as_bytes(absolute_file_path))
-			file.close()
-	for dir_name in dir.get_directories():
-		var dir_path = relative_path + dir_name + '/'
-		var absolute_dir_path = directory_path + "/" + dir_name
-		add_files_to_zip(zip_packer, absolute_dir_path, dir_path)
-	return OK
+    var dir = DirAccess.open(directory_path)
+    for file_name in dir.get_files():
+        var file_path = relative_path + file_name
+        print(file_path)
+        var absolute_file_path = directory_path + "/" + file_name
+        var err = zip_packer.start_file(file_path)
+        if err != OK:
+            return err
+        
+        var file := FileAccess.open(absolute_file_path, FileAccess.READ)
+        if file:
+            zip_packer.write_file(FileAccess.get_file_as_bytes(absolute_file_path))
+            file.close()
+    for dir_name in dir.get_directories():
+        var dir_path = relative_path + dir_name + '/'
+        var absolute_dir_path = directory_path + "/" + dir_name
+        add_files_to_zip(zip_packer, absolute_dir_path, dir_path)
+    return OK
 
 static func find_files_by_extension(extension, path = "res://"):
-	var dir = DirAccess.open(path)
-	var files = []
-	for file_name in dir.get_files():
-		if file_name.to_lower().ends_with(extension):
-			files.append(file_name)
-	return files
+    var dir = DirAccess.open(path)
+    var files = []
+    for file_name in dir.get_files():
+        if file_name.to_lower().ends_with(extension):
+            files.append(file_name)
+    return files
 
 static func get_csproj_content(csproj_path):
-	var file := FileAccess.open(csproj_path, FileAccess.READ)
-	assert(file, "csproj file could not be opened: " + csproj_path)
+    var file := FileAccess.open(csproj_path, FileAccess.READ)
+    assert(file, "csproj file could not be opened: " + csproj_path)
 
-	var csproj_content = file.get_as_text()
-	file.close()
-	return csproj_content
+    var csproj_content = file.get_as_text()
+    file.close()
+    return csproj_content
 
 static func csproj_planetary_reference_exists(csproj_path):
-	_init_static_variables()
-	var csproj_content = get_csproj_content(csproj_path)
-	return csproj_content.find(reference_tag) != -1 and csproj_content.find(reference_hint) != -1
+    _init_static_variables()
+    var csproj_content = get_csproj_content(csproj_path)
+    return csproj_content.find(reference_tag) != -1 and csproj_content.find(reference_hint) != -1
 
 static func add_planetary_csproj_ref(csproj_path):
-	_init_static_variables()
-	var csproj_content = get_csproj_content(csproj_path)
+    _init_static_variables()
+    var csproj_content = get_csproj_content(csproj_path)
 
-	var item_group_exists = csproj_content.find("<ItemGroup>") != -1
-	var reference_exists = csproj_planetary_reference_exists(csproj_path)
-	var reference_string = reference_tag + "\n      " + reference_hint + "\n    </Reference>\n"
-	if item_group_exists and reference_exists:
-		return
-	if not item_group_exists:
-		var item_group_tag = "  <ItemGroup>\n    " + reference_string + "  </ItemGroup>"
-		csproj_content = csproj_content.replace("</PropertyGroup>", "</PropertyGroup>\n" + item_group_tag)
-	elif not reference_exists:
-		var item_group_pos = csproj_content.find("<ItemGroup>")
-		var item_group_end_pos = csproj_content.find("</ItemGroup>", item_group_pos)
-		csproj_content = csproj_content.insert(item_group_end_pos, "  " + reference_string + "  ")
+    var item_group_exists = csproj_content.find("<ItemGroup>") != -1
+    var reference_exists = csproj_planetary_reference_exists(csproj_path)
+    var reference_string = reference_tag + "\n      " + reference_hint + "\n    </Reference>\n"
+    if item_group_exists and reference_exists:
+        return
+    if not item_group_exists:
+        var item_group_tag = "  <ItemGroup>\n    " + reference_string + "  </ItemGroup>"
+        csproj_content = csproj_content.replace("</PropertyGroup>", "</PropertyGroup>\n" + item_group_tag)
+    elif not reference_exists:
+        var item_group_pos = csproj_content.find("<ItemGroup>")
+        var item_group_end_pos = csproj_content.find("</ItemGroup>", item_group_pos)
+        csproj_content = csproj_content.insert(item_group_end_pos, "  " + reference_string + "  ")
 
-	write_string_to_file(csproj_path, csproj_content)
+    write_string_to_file(csproj_path, csproj_content)
